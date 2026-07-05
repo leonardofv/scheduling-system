ba64d0b69e1db60b7b060714e68331ff19818882
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-07-05 13:12:58 -0300

    add logout route

--- server/routes/api.php
+++ server/routes/api.php
@@ -16,6 +16,7 @@ Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:au
 //Qualquer usuário autenticado
 Route::middleware('auth:sanctum')->group(function() {
 
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
